5ffb0dd588e2e65a10090acbd5e1ebecc4157bb2
Michael Patterson <mjpatter88@gmail.com>
2014-02-09 14:44:22 -0600

    Solutions to exercises from chapter 00.

---
+++ chapters/00/hwprinter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"//a small c++ program"<<std::endl;
+    std::cout<<"#include <iostream>"<<std::endl;
+    std::cout<<""<<std::endl;
+    std::cout<<"int main()"<<std::endl;
+    std::cout<<"{"<<std::endl;
+    std::cout<<"\tstd::cout<<\"Hello, World!\"<<std::endl;"<<std::endl;
+    std::cout<<"\treturn 0;"<<std::endl;
+    std::cout<<"}"<<std::endl;
+    return 0;
+}
